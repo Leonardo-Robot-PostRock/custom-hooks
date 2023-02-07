05b76852791887e54ca86d0aeadfa4511d511f4c
Leonardo Puebla <leonardo38361@gmail.com>
2023-02-06 21:09:59 -0300

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,45 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodos = (initialState = []) => {
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+        return () => { };
+    }, [todos]);
+
+    const handleNewTodo = (todo) => {
+        const action = { type: 'Add Todo', payload: todo };
+        dispatchTodo(action);
+    };
+
+    const handleDeleteTodo = (id) => {
+        dispatchTodo({
+            type: 'Remove Todo',
+            payload: id
+        });
+    };
+
+    const handleToggleTodo = (id) => {
+        console.log({ id });
+        dispatchTodo({
+            type: 'Toggle Todo',
+            payload: id
+        });
+    };
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter((todo) => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo
+    }
+}
